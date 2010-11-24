f0c9ca54ad0629a190561e9ba96f7445f68e4522
jschmid <jonas.schmid@gmail.com>
2010-11-24 19:30:36 +0000

    Affichage de l'étoile sur l'activity principale
    git-svn-id: https://svn.epfl.ch/svn/snowreport/SnowReport@52 2a11e49e-03e7-4c15-ae42-62b212d7452a

--- src/pro/schmid/android/snowreport/view/ResortAdapter.java
+++ src/pro/schmid/android/snowreport/view/ResortAdapter.java
@@ -3,23 +3,29 @@ package pro.schmid.android.snowreport.view;
 import java.util.List;
 
 import pro.schmid.android.snowreport.R;
+import pro.schmid.android.snowreport.model.FavoritesManager;
 import pro.schmid.android.snowreport.model.Resort;
-import android.content.Context;
+import android.app.Activity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class ResortAdapter extends ArrayAdapter<Resort> {
 
 	private List<Resort> resorts;
 	private LayoutInflater mInflater;
+	private Activity activity;
 
-	public ResortAdapter(Context context, int textViewResourceId, List<Resort> items) {
-		super(context, textViewResourceId, items);
+	public ResortAdapter(Activity activity, int textViewResourceId, List<Resort> items) {
+		super(activity, textViewResourceId, items);
+		
 		this.resorts = items;
-		mInflater = LayoutInflater.from(context);
+		this.activity = activity;
+		
+		mInflater = LayoutInflater.from(activity);
 	}
 
 	@Override
@@ -28,7 +34,7 @@ public class ResortAdapter extends ArrayAdapter<Resort> {
 		if (v == null) {
 			v = mInflater.inflate(R.layout.resort_item, null);
 		}
-		Resort r = resorts.get(position);
+		final Resort r = resorts.get(position);
 		if (r != null) {
 			TextView tv;
 			
@@ -65,6 +71,16 @@ public class ResortAdapter extends ArrayAdapter<Resort> {
 			tv = (TextView) v.findViewById(R.id.lastUpdate);
 			if(tv != null){
 				tv.setText(r.getLastUpdate());
+			}
+			
+			ImageView i = (ImageView) v.findViewById(R.id.favoriteImage);
+			
+			if(i != null) {
+				if(FavoritesManager.isFavorite(activity, r.getId())) {
+					i.setImageResource(R.drawable.star_on);
+				} else {
+					i.setImageResource(R.drawable.star_off);
+				}
 			}
 		}
 		
